1600f1b9130317fb082bcc3b3f3dd226b823e9de
zeritte <zeritte@gmail.com>
2022-08-14 19:06:18 +0100

    simple navigation setup end

--- App.js
+++ App.js
@@ -2,40 +2,51 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import HomeScreen from './src/screens/SearchScreen';
+import SearchScreen from './src/screens/SearchScreen';
 import PublishScreen from './src/screens/PublishScreen';
 import MyTripsScreen from './src/screens/MyTripsScreen';
 import InboxScreen from './src/screens/InboxScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import SelectLocationScreen from './src/screens/SelectLocationScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+function SearchStackScreen() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="SearchInsideStack" component={SearchScreen} />
+      <Stack.Screen name="SearchLocation" component={SelectLocationScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
+      // TODO: solve react native vector icons issue
+      // screenOptions={({route}) => ({
+      //   tabBarIcon: ({focused, color, size}) => {
+      //     let iconName;
 
-            if (route.name === 'Sarch') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Publish') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
-            }
+      //     if (route.name === 'Sarch') {
+      //       iconName = focused
+      //         ? 'ios-information-circle'
+      //         : 'ios-information-circle-outline';
+      //     } else if (route.name === 'Publish') {
+      //       iconName = focused ? 'ios-list-box' : 'ios-list';
+      //     }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}>
-        <Tab.Screen name="Search" component={HomeScreen} />
+      //     // You can return any component that you like here!
+      //     return <Ionicons name={iconName} size={size} color={color} />;
+      //   },
+      //   tabBarActiveTintColor: 'tomato',
+      //   tabBarInactiveTintColor: 'gray',
+      // })}
+      >
+        <Tab.Screen name="Search" component={SearchStackScreen} />
         <Tab.Screen name="Publish" component={PublishScreen} />
         <Tab.Screen name="My Trips" component={MyTripsScreen} />
         <Tab.Screen name="Inbox" component={InboxScreen} />
