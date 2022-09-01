8dd135b1122b7bd16bb3d706f33391c3a2fd6e27
Akif Ünlü <akif.unlu44@gmail.com>
2022-09-02 01:08:48 +0300

    Marker added to maps (Ankara and Malatya)

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -18,6 +18,16 @@ export default function () {
           longitudeDelta: 0.0421
         }}
       >
+        <Marker
+          coordinate={{ latitude: 38.355363, longitude: 38.333525 }}
+          title={"title"}
+          description={"Malatya"}
+        />
+        <Marker
+          coordinate={{ latitude: 39.866667, longitude: 32.866667 }}
+          title={"title"}
+          description={"Ankara"}
+        />
       </MapView>
     </View>
   );
