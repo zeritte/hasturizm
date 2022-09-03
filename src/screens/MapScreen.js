2a979aa1cc7eadd837c5f2002ca1b0435df43f2d
Akif Ünlü <akif.unlu44@gmail.com>
2022-09-03 16:21:43 +0300

    Location information assigned to const

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -2,34 +2,32 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 
+const MALATYA_LOCATION = {latitude: 38.355363, longitude: 38.333525}
+const ANKARA_LOCATION = {latitude: 39.866667, longitude: 32.866667}
+const INITIAL_DELTAS = {latitudeDelta: 0.0922, longitudeDelta: 0.0421}
+
+initialRegion = {... MALATYA_LOCATION, ... INITIAL_DELTAS}
+
 export default function () {
   return (
     <View>
       <Text>Map Screen</Text>
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 38.355363,
-          longitude: 38.333525,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
+        initialRegion={initialRegion}
       >
         <Polyline
-          coordinates={[
-            { latitude: 38.355363, longitude: 38.333525 },
-            { latitude: 39.866667, longitude: 32.866667 }
-          ]}
+          coordinates={[MALATYA_LOCATION, ANKARA_LOCATION]}
           strokeColor="red"
           strokeWidth={6}
         />
         <Marker
-          coordinate={{ latitude: 38.355363, longitude: 38.333525 }}
+          coordinate={MALATYA_LOCATION}
           title={'title'}
           description={'Malatya'}
         />
         <Marker
-          coordinate={{ latitude: 39.866667, longitude: 32.866667 }}
+          coordinate={ANKARA_LOCATION}
           title={'title'}
           description={'Ankara'}
         />
