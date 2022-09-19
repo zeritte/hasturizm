b19caa45d624a71713795a092112eb922becebcf
Akif Ünlü <akif.unlu44@gmail.com>
2022-09-19 17:42:00 +0300

    Tring GooglePlacesAutocomplete

--- src/screens/SelectLocationScreen.js
+++ src/screens/SelectLocationScreen.js
@@ -1,6 +1,8 @@
 import { Button, SearchBar } from "@rneui/themed";
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import config from "react-native-ultimate-config";
 
 export default function ({ navigation }) {
   const [search, setSearch] = useState("");
@@ -8,23 +10,38 @@ export default function ({ navigation }) {
     setSearch(search);
   };
   return (
-    <View>
-      <Text>Select Location Screen</Text>
-      <SearchBar
+    // <View>
+    //   <Text>Select Location Screen</Text>
+
+      <GooglePlacesAutocomplete
+        placeholder="SearchMalatya"
+        autoFillOnNotFound={false}
+        onFail={console.warn}
+        onNotFound={console.warn}
+        onPress={(data, details = null) => {
+          console.log(data, details);
+        }}
+        query={{
+          key: config.REACT_NATIVE_MAP_API_KEY,
+          
+        }}
+      />
+  )
+      {/* <SearchBar
         placeHolder="Type here"
         onChangeText={updateSearch}
         value={search}
         lightTheme={true}
         text={search}
-      />
-      <Button
+      /> */}
+      {/* <Button
         onPress={() => navigation.navigate("SearchInsideStack")}
         style={styles.button}
         flexWrap="wrap"
         title="Select"
-      />
-    </View>
-  );
+      /> */}
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
