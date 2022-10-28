eca60299133b2622c17b776b9c77fb0fcf817d79
Akif <akif.unlu44@gmail.com>
2022-10-28 17:19:28 +0300

    preSearcArr converted to dynamic

--- src/components/PreviousSearches.js
+++ src/components/PreviousSearches.js
@@ -10,28 +10,10 @@ import { previousSearchesSelector } from '../lib/selectors';
 export default function PreviousSearches() {
   const added = useSelector(previousSearchesSelector);
 
-  const preSearchArr = [
-    {
-      title: `${added[0]?.depreture.name}   =>   ${added[0]?.arrival.name}`,
-      id: 1,
-    },
-    {
-      title: `${added[1]?.depreture.name}   =>   ${added[1]?.arrival.name}`,
-      id: 2,
-    },
-    {
-      title: `${added[2]?.depreture.name}   =>   ${added[2]?.arrival.name}`,
-      id: 3,
-    },
-    {
-      title: `${added[3]?.depreture.name}   =>   ${added[3]?.arrival.name}`,
-      id: 4,
-    },
-    {
-      title: `${added[4]?.depreture.name}   =>   ${added[4]?.arrival.name}`,
-      id: 5,
-    },
-  ];
+  const preSearchArr = added.map((item, index) => ({
+    title: `${added[index]?.depreture.name ?? ' '}        ${added[index]?.arrival.name ?? ' '}`,
+    id: 1,
+  }));
 
   const data = preSearchArr.slice(0, 5);
   console.log(data);
