45d76ce84aff185e89e10bbad23019d295f2cda3
Akif <akif.unlu44@gmail.com>
2022-11-06 21:05:18 +0300

    previous searches appearance fixed

--- src/components/PreviousSearches.js
+++ src/components/PreviousSearches.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable no-plusplus */
 import { Card } from '@rneui/base';
 import React, { useMemo } from 'react';
 import {
@@ -14,35 +12,30 @@ function PreviousSearches() {
   const previousSearches = useSelector(previousSearchesSelector);
 
   const data = useMemo(() => previousSearches.map((item) => ({
+    departure: `${item?.departure?.name ?? ' '}`,
+    arrival: `${item?.arrival?.name ?? ' '}`,
+    passenger: `Passenger: ${item?.passengerCount ?? ' '}`,
+    date: `Date: ${item?.date?.getDate()}.${item?.date?.getMonth() + 1}.${item?.date?.getFullYear()}`,
+    id: item.id,
   })).slice(0, 5), [previousSearches]);
 
   return (
-    <SafeAreaView style={{ marginBottom: 1000 }}>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <Card borderRadius={30}>
-            <View style={{
-              flexDirection: 'row',
-            }}
-            >
-              <Ionicons style={styles.icon} name="time-outline" />
+    <>
+      {data.map((item) => (
+        <Card borderRadius={30} key={item.id}>
+          <View style={styles.view}>
+            <Ionicons style={styles.icon} name="time-outline" />
             <Text style={styles.item}>{item.departure}</Text>
-              <Ionicons style={styles.icon} name="arrow-forward-outline" />
-              <Text style={styles.item}>{item.arrival}</Text>
-            </View>
-            <View style={{
-              flexDirection: 'row',
-            }}
-            >
-              <Text style={styles.item}>{item.date}</Text>
-              <Text style={styles.item}>{item.person}</Text>
-            </View>
-          </Card>
-        )}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
+            <Ionicons style={styles.icon} name="arrow-forward-outline" />
+            <Text style={styles.item}>{item.arrival}</Text>
+          </View>
+          <View style={styles.view}>
+            <Text style={styles.item}>{item.date}</Text>
+            <Text style={styles.item}>{item.passenger}</Text>
+          </View>
+        </Card>
+      ))}
+    </>
   );
 }
 
@@ -67,5 +60,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
     fontSize: 25,
     flex: 0.2,
+  },
+  view: {
+    flexDirection: 'row',
   },
 });
