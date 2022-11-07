e7f7b8d7dac8a39381dd7c1ae9be3a5bf691c0a1
Akif <akif.unlu44@gmail.com>
2022-11-07 22:55:47 +0300

    Search Result Screen page created

---
+++ src/screens/SearchResultScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SearchResultScreen() {
+  return (
+    <View>
+      <Text>Search Result Screen</Text>
+    </View>
+  );
+}
