4554ea05e7cc125b669edd15af4182d8657ad658
Akif <akif.unlu44@gmail.com>
2022-11-10 20:32:14 +0300

    Search result screen card created without real data

--- src/screens/SearchResultScreen.js
+++ src/screens/SearchResultScreen.js
@@ -1,10 +1,66 @@
+import { Card } from '@rneui/base';
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function SearchResultScreen() {
+export default function SearchResultScreen({ navigation }) {
   return (
-    <View>
-      <Text>Search Result Screen</Text>
-    </View>
+    <>
+      <Card borderRadius={30}>
+        <View alignItems="center" flexDirection="row">
+          <TouchableOpacity onPress={() => navigation.navigate('SearchInsideStack')}>
+            <Ionicons style={styles.icon} name="chevron-back-outline" />
+          </TouchableOpacity>
+          <View flexDirection="column">
+            <View flexDirection="row">
+              <Text style={styles.item}>Departure</Text>
+              <Ionicons style={styles.icon} name="arrow-forward-outline" />
+              <Text style={styles.item}>Arrival</Text>
+            </View>
+            <View flexDirection="row">
+              <Text style={styles.subItem}>Date ,</Text>
+              <Text style={styles.subItem}>Passenger</Text>
+            </View>
+          </View>
+        </View>
+      </Card>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  item: {
+    textAlign: 'center',
+    alignItems: 'center',
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    fontSize: 20,
+  },
+  subItem: {
+    textAlign: 'center',
+    alignItems: 'center',
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    fontSize: 17,
+  },
+  cardItem: {
+    borderRadius: 30,
+    backgroundColor: 'blue',
+  },
+  icon: {
+    textAlign: 'center',
+    alignItems: 'center',
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    fontSize: 30,
+  },
+  view: {
+    flexDirection: 'row',
+  },
+});
+
